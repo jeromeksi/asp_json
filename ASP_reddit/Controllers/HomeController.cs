883aeb0a51c871c4de108dd16b0b59ed485c4a33
Jérôme KSIGZKIEWIEZ <41283804+jeromeksi@users.noreply.github.com>
2019-07-12 15:51:13 +0200

    UPDATE bootstrap

--- ASP_reddit/Controllers/HomeController.cs
+++ ASP_reddit/Controllers/HomeController.cs
@@ -13,45 +13,22 @@ namespace ASP_reddit.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            Models.RedditList redditTitle= new Models.RedditList();            
+            return View();
+        }
+        public ActionResult ViewReddit()
+        {
+            Models.RedditList redditTitle = new Models.RedditList();
             JObject json = JObject.Parse(new WebClient().DownloadString("https://www.reddit.com/r/programming/.json"));
             JToken selectData = json.SelectToken("$.data.children");
 
-            foreach(JObject childrend  in selectData)
+            foreach (JObject childrend in selectData)
             {
                 redditTitle.addTitle(childrend.SelectToken("data.title").ToString(),
-                                    childrend.SelectToken("data.permalink").ToString(),    
+                                    childrend.SelectToken("data.permalink").ToString(),
                                     childrend.SelectToken("data.url").ToString()
                     );
             }
             return View(redditTitle);
         }
-/*
-JEnumerable<JToken> vv =  json.Children();
-JToken data = vv.ElementAt(1);
-vv = data.Children();
-
-foreach(JToken aa in vv.Children())
-{
-    if(aa.Path == "data.children")
-    {
-        JEnumerable<JObject> vvvv = aa.Children().ElementAt(0).Children<JObject>();//
-
-
-        foreach (JObject xx in vvvv)
-        {
-
-            JToken jt = xx.SelectToken("$.data[?(@.Name == 'title')]");
-            JEnumerable<JObject> m = xx.Children().ElementAt(0).Children<JObject>();
-            JObject title = m.Select(x => x.N);
-            title
-        }
-
-        string s = vvvv.Values("title").ToString();
-    }
-}
-
-*/
-
     }
 }
